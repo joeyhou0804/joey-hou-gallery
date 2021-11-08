96bcb9f0437466488cc653129ccc35246813c9f8
JoeyHou <jh4170@columbia.edu>
2021-11-07 20:38:46 -0500

    debug

--- schemas.js
+++ schemas.js
@@ -6,6 +6,7 @@ module.exports.stationSchema = Joi.object({
         // image: Joi.string().required(),
         date: Joi.string().required(),
         train: Joi.string(),
+        haveMap: Joi.boolean(),
         description: Joi.string(),
     }).required(),
     deleteImages: Joi.array()
