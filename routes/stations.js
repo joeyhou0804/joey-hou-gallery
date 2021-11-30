aad40e8427d7d7219904778bad088c5ed55bfe36
JoeyHou <jh4170@columbia.edu>
2021-11-30 02:06:14 -0500

    delete

--- routes/stations.js
+++ routes/stations.js
@@ -5,8 +5,6 @@ const ExpressError = require('../utils/ExpressError');
 const Station = require('../models/station');
 
 const multer = require('multer');
-const {storage} = require('../cloudinary');
-const upload = multer({storage});
 const stations = require('../controllers/stations');
 
 
@@ -14,13 +12,11 @@ const stations = require('../controllers/stations');
 
 router.route('/')
     .get(catchAsync(stations.index))
-    .post(upload.array('image'), catchAsync(stations.createStation));
 
 router.get('/new', stations.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(stations.showStation))
-    .put(upload.array('image'), catchAsync(stations.updateStation))
     .delete(catchAsync(stations.deleteStation));
 
 router.get('/:id/edit', catchAsync(stations.renderEditForm))
