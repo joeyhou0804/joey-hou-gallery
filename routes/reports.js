4312507a8e0136c17a7c33223c5f2eccad6b3513
Joey Hou <joeyhou0804@gmail.com>
2021-12-12 23:42:43 -0500

    added reports

---
+++ routes/reports.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const videos = require('../controllers/videos');
+
+//===========================================
+
+router.route('/')
+    .get(catchAsync(reports.index));
+
+module.exports = router;
