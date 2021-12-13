fd16343fb525c605ffda00afb8721b6a7fc6fbb8
Joey Hou <joeyhou0804@gmail.com>
2021-12-12 23:45:03 -0500

    fix bugs

--- routes/reports.js
+++ routes/reports.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
-const videos = require('../controllers/videos');
+const reports = require('../controllers/reports');
 
 //===========================================
 
