62790bff4ec827ac457f03146be8d0bb777f3d64
Joey Hou <joeyhou0804@gmail.com>
2022-09-19 22:29:14 -0700

    Debug applications

--- routes/applications.js
+++ routes/applications.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
-const arts = require('../controllers/applications');
+const applications = require('../controllers/applications');
 
 //===========================================
 
